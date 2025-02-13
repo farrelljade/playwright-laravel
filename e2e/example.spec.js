14d36a0f1ab1f05b518820c97f95a3f8c549e2e2
farrelljade <farrellmikel@yahoo.com>
2025-02-13 21:42:12 +0000

    Create first test for logging into application

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -1,19 +1,20 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has laracasts', async ({ page }) => {
+    await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+    await expect(page.locator('body')).toContainText(/Laracasts/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('log in', async ({ page }) => {
+    const body = (page.locator('body'));
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await page.goto('/');
+    await page.getByRole('link', { name: 'Log in' }).click();
+    await expect(body).toContainText(/Email/);
+    await page.fill('#email', 'admin@admin.com');
+    await page.fill('#password', 'password');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await expect(body).toContainText(/You're logged in!/);
 });
